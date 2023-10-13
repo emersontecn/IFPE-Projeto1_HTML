62bdc9a363286abcdfa56c70d47b09fedf820726
emersontecn <emerson0712002@gmail.com>
2023-10-13 13:21:41 -0300

    Atualizado

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function menushow(){
+    let menumobile = document.querySelector('.menu-mobile');
+
+    if (menumobile.classList.contains('open')){
+        menumobile.classList.remove('open');
+        document.querySelector('.icon').src = "img/menu_white_36dp.svg";
+    } else{
+        menumobile.classList.add('open');
+        document.querySelector('.icon').src = "img/close_white_36dp.svg";
+    }
+}
